0baed2b84eb9ca1ab762948d452fb4acf797950e
VyacheslawDekin <vyacheslaw.dekin@yandex.ru>
2022-07-16 23:31:57 +0300

    added Relationship in models.py, added signals

--- onChill/profiles/models.py
+++ onChill/profiles/models.py
@@ -38,4 +38,21 @@ class Profile(models.Model):
         super(Profile, self).save(force_insert, force_update, using, update_fields)
 
 
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.created.strftime('%d-%m-%Y')}"
+
+
 
